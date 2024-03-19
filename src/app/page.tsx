28075c27f9d231bfcbb2b511dd943fc2a33a1c3c
Vojtech Cerveny <cervik49@gmail.com>
2024-03-20 00:16:23 +0100

    feat: adding header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,15 @@ import { MyTab } from "./my-tab";
 
 export default async function Home() {
   const response = await getTOP10Senders("g00dsic9bw4");
-  if(!response.success) {
+  if (!response.success) {
     return <div>Error: {response.message}</div>;
   }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col min-h-screen p-24 ">
+      <div className="z-10 max-w-5xl w-full font-mono text-sm lg:flex flex-col container">
+        <h1 className="text-4xl font-bold max-w-5xl">Animal donation</h1>
+        <h2 className="text-3xl font-bold max-w-5xl text-gray-600">TOP 10 donators</h2>
+      </div>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex container">
         <Tabs defaultValue="animal-donation" className="w-full">
           <TabsList className="grid w-full grid-cols-2">
